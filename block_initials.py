177db58d7f91e216490a965d4859202d7c952d07
Milk <56483559+versacemilk@users.noreply.github.com>
2022-03-28 05:53:25 -0700

    Add files via upload

---
+++ block_initials.py
@@ -0,0 +1,34 @@
+#Milk Man
+#I am the radest
+
+initials = """
+M   M M   M
+MM MM MM MM
+MM MM MM MM
+M M M M M M
+M   M M   M
+M   M M   M
+M   M M   M
+"""
+
+
+first_initial = """
+M   M
+MM MM
+MM MM
+M M M
+M   M
+M   M
+M   M
+"""
+
+second_initial = """
+M   M
+MM MM
+MM MM
+M M M
+M   M
+M   M
+M   M
+"""
+print(initials)
