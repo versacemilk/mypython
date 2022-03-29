d4a4288f070b71f8ba63c744bb349e34f8d5679f
Milk <56483559+versacemilk@users.noreply.github.com>
2022-03-29 06:08:37 -0700

    Add files via upload

---
+++ userinput1.py
@@ -0,0 +1,9 @@
+from tkinter import N
+
+
+like_snakes = input("Do you like snakes?")
+if like_snakes == 'yes' or 'Yes':
+    print("Hell yea brother, Snakes are very cool!")
+    
+else:
+	print("Bummer")
