009d00c570c6a555ac029f82c360bee065e4043e
Milk <56483559+versacemilk@users.noreply.github.com>
2022-03-29 21:32:04 -0700

    Add files via upload
    fixed line 2

--- userinput1.py
+++ userinput1.py
@@ -1,6 +1,5 @@
 like_snakes = input("Do you like snakes?")
-if like_snakes == 'yes' or 'Yes':
-	print("Hell yea brother, Snakes are very cool!")
-    
+if like_snakes == 'Yes' or like_snakes == 'yes':
+  print("Hell yea brother, Snakes are very cool!")
 else:
-	print("Bummer")
+  print("Bummer")
